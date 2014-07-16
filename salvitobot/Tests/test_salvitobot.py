32e2821025e18df21712a07581ab0e0e75dec16c
aniversarioperu <aniversarioperu1@gmail.com>
2014-07-16 22:29:57 +0300

    main function for test

--- salvitobot/Tests/test_salvitobot.py
+++ salvitobot/Tests/test_salvitobot.py
@@ -9,6 +9,12 @@ class SalvitoTest(unittest.TestCase):
     def test_data_extractor_magnitude(self):
         json_file = "1405369502.25.json"
         magnitude = "5.5"
-        result = DataExtractor("http://aniversarioperu.me/salvitobot/1405371303.23.json")
-        print result.items
+        url = "http://aniversarioperu.me/salvitobot/1405371303.23.json"
+        result = DataExtractor(url)
+        print result.get_items()
         self.assertEqual(magnitude, result)
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity = 2)
+    unittest.main(testRunner=runner)
