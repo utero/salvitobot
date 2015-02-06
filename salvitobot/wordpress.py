0c0f0fc907ee258f80d54e0c3fc680d9fe4b1617
aniversarioperu <aniversarioperu1@gmail.com>
2015-02-06 23:49:08 +0200

    raise exceptioon when wordpress is not configured

--- salvitobot/wordpress.py
+++ salvitobot/wordpress.py
@@ -3,9 +3,23 @@ from wordpress_xmlrpc import WordPressPost
 from wordpress_xmlrpc.methods import posts
 
 from . import config
+from .exceptions import WordPressNotConfigured
 
 
 def post_to_wp(title, content):
+    msg = "\nYou need to set up your WordPress credentials: \n" \
+          "Use:\n" \
+          "    salvitobot.config.wordpress_client = 'https://mydomain.wordpress.com/xmlrpc.php'\n" \
+          "    salvitobot.config.wordpress_username = 'yourusername'\n" \
+          "    salvitobot.config.wordpress_password = 'yourpassword'\n"
+
+    if config.wordpress_client == '':
+        raise WordPressNotConfigured(msg)
+    if config.wordpress_username == '':
+        raise WordPressNotConfigured(msg)
+    if config.wordpress_password == '':
+        raise WordPressNotConfigured(msg)
+
     wp = Client(config.wordpress_client, config.wordpress_username, config.wordpress_password)
     post = WordPressPost()
     post.title = title
