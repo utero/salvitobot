212b7c18d9ab7bae3b882447a49891a9cc5db8d3
aniversarioperu <aniversarioperu1@gmail.com>
2015-02-19 13:27:30 +0200

    0.3.0

--- salvitobot/__init__.py
+++ salvitobot/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'aniversarioperu'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 from .api import Bot
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ test_requirements = [
 
 setup(
     name="salvitobot",
-    version="0.2.0",
+    version="0.3.0",
     url="https://github.com/aniversarioperu/salvitobot",
 
     author="AniversarioPeru",
