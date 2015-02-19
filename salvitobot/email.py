ec422f5bceb30b4c3e6f23fcdf3729be0cd24928
aniversarioperu <aniversarioperu1@gmail.com>
2015-02-19 10:44:26 +0200

    use encoded headers

--- salvitobot/email.py
+++ salvitobot/email.py
@@ -1,26 +1,42 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email import charset
+from email.charset import Charset
+from email.generator import Generator
+import io
 
 
 def send(email_receivers, stories):
-    sender = 'salvitobot@salvitobot.com'
+    charset.add_charset('utf-8', charset.QP, charset.QP)
+
+    sender = 'salvitobot'
 
     for story in stories:
-        message = ''
+        for recipient in email_receivers:
+            multipart = MIMEMultipart('alternative')
+            multipart['Subject'] = Header(story['title'].encode('utf-8'), 'UTF-8').encode()
+            multipart['To'] = Header(recipient.encode('utf-8'), 'UTF-8').encode()
+            multipart['From'] = Header(sender.encode('utf-8'), 'UTF-8').encode()
 
-        message += "From: Salvitobot <" + sender + ">\n"
-        message += "To: <" + '><'.join(email_receivers) + ">\n"
-        message += "Subject: " + story['title'] + "\n\n"
+            textpart = MIMEText(story['body'], 'plain', 'UTF-8')
+            multipart.attach(textpart)
 
-        message += story['body'] + "\n"
+            mystream = io.StringIO()
+            g = Generator(mystream, False)
+            g.flatten(multipart)
 
-        try:
-            smtpObj = smtplib.SMTP('localhost')
-            smtpObj.sendmail(sender, email_receivers, message.encode('utf-8'))
-            message_to_user = "\nSuccessfully sent email.\n" \
-                              "Probably it went to your SPAM folder.\n" \
-                              "If you are using Gmail you might need to create a filter\n" \
-                              "to avoid sending emails from <salvitobot@salvitobot.com> to \n" \
-                              "your SPAM folder. More info here https://support.google.com/mail/answer/6579\n"
-            print(message_to_user)
-        except smtplib.SMTPException:
-            print("Error: unable to send email")
+            message = mystream.getvalue()
+
+            try:
+                smtpObj = smtplib.SMTP('localhost')
+                smtpObj.sendmail(sender, recipient, message)
+                message_to_user = "\nSuccessfully sent email.\n" \
+                                  "Probably it went to your SPAM folder.\n" \
+                                  "If you are using Gmail you might need to create a filter\n" \
+                                  "to avoid sending emails from <salvitobot@aniversarioperu.me> to \n" \
+                                  "your SPAM folder. More info here https://support.google.com/mail/answer/6579\n"
+                print(message_to_user)
+            except smtplib.SMTPException:
+                print("Error: unable to send email")
